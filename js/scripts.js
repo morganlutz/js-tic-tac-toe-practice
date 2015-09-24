070107c4389253debb6f3ee0c4715a6dec8313ff
diana & mala <student@epicodus.com>
2015-09-24 12:29:57 -0700

    Add method to mark squares on game board. Not yet connected to spaces.

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ function Space(xCoordinate, yCoordinate) {
   Space.prototype.markByPlayer = function(player) {
     if(this.checkIfEmpty()) {
       this.markedBy = player.mark;
+
     } else {
 
     }
@@ -27,6 +28,14 @@ function Space(xCoordinate, yCoordinate) {
   Space.prototype.getMark = function() {
     return this.markedBy;
   }
+
+
+// Use this later to draw X and O on the board
+function mark(space) {
+  var square = document.getElementById(space).getContext("2d");
+  square.font = "270px Arial";
+  square.fillText("x", 90, 145);
+}
 
 function createBoard(numberOfSpaces) {
     this.board = [];
@@ -48,8 +57,6 @@ function createBoard(numberOfSpaces) {
 
 
   createBoard.prototype.isWinner = function() {
-    //debugger;
-
     if( this.findSpace(0, 0).markedBy === this.findSpace(0, 1).markedBy && this.findSpace(0, 1).markedBy === this.findSpace(0, 2).markedBy)//one
     if( this.findSpace(1, 0).markedBy === this.findSpace(1, 1).markedBy && this.findSpace(1, 1).markedBy === this.findSpace(1, 2).markedBy)//two
     if( this.findSpace(2, 0).markedBy === this.findSpace(2, 1).markedBy && this.findSpace(2, 1).markedBy === this.findSpace(2, 2).markedBy)//three
@@ -83,15 +90,24 @@ function Game(number) {
   }
 
 
+$(document).ready(function() {
+    //spaces in row one:
+    var square1 = document.getElementById("0,0").getContext("2d");
+    var square2 = document.getElementById("0,1").getContext("2d");
+    var square3 = document.getElementById("0,2").getContext("2d");
 
-// $(document).ready(function() {
-//   $("form#").submit(function(event) {
-//     //var number = parseInt($("input#number").val());
-//
-//     $(".answer").text(result);
-//
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
+    //spaces in row two:
+    var square4 = document.getElementById("1,0").getContext("2d");
+    var square5 = document.getElementById("1,1").getContext("2d");
+    var square6 = document.getElementById("1,2").getContext("2d");
+
+    //spaces in row three:
+    var square7 = document.getElementById("2,0").getContext("2d");
+    var square8 = document.getElementById("2,1").getContext("2d");
+    var square9 = document.getElementById("2,2").getContext("2d");
+
+
+    $(".winner").text(result);
+    $("#result").show();
+    event.preventDefault();
+});
